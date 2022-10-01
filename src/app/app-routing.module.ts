da0ad27b8cf31937bb7097297e2a361eb0a36045
giaenuuu <git@gruee.ch>
2022-10-01 21:13:54 +0200

    Add Basic Routing and Core/Shared Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeRoutingModule } from './home/home-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => HomeRoutingModule },
+  { path: '', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
