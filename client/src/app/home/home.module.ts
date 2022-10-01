f2abf37708be266612fdc934140eb0b2cf94cfcc
Gian <git@gruee.ch>
2022-10-01 23:47:45 +0200

    Add header navigation to application (#1)

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ],
-  declarations: [HomeComponent]
+  exports: [SharedModule],
+  declarations: [HomeComponent],
+  providers: [SharedModule]
 })
 export class HomeModule { }
