b66e20ec6b532b0248e2515d5a9f912463b7bc9d
Fl3xm3ist3r <artopien.net@gmail.com>
2022-10-02 04:32:20 +0200

    comment component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommentType } from '../shared/comment/comment.component';
+import { helloWorldComment } from 'src/assets/utf-styles/hello_world';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public commentType = CommentType;
+  public helloWorldCommentValue = helloWorldComment;
+  public splitPositionValue = [19];
 
   constructor() { }
 
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    CommentComponent
+  ],
+  exports: [
+    CommentComponent
+  ],
+  providers: [
+    CommentComponent
+  ]
 })
 export class SharedModule { }
