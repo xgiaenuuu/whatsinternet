9d8eda15919aa08cfa2d9e11c7345a3661ec101d
giaenuuu <git@gruee.ch>
2022-10-08 02:12:29 +0200

    remove server and DB

--- server/src/controllers/_base.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-
-export default abstract class Controller {
-    public abstract path: string;
-    public abstract router: Router;
-    public abstract intializeRoutes(): void;
-}
--- server/src/domain/_dbconfig.ts
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from 'sequelize';
-require('dotenv').config();
-
-export const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: process.env.DB,
-});
